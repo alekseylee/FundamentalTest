888b70a3325cbfcf2e2fc99b5a583237456e40bd
AAA <aleksey.lee@gmail.com>
2022-12-02 11:57:02 +0100

    init commit

--- src/Dierenopvang/AnimalShelter.java
+++ src/Dierenopvang/AnimalShelter.java
@@ -5,23 +5,14 @@ import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class AnimalShelter {
-    private List<Animal> animals;
+    private List<Animal> animals = new ArrayList<>();
     private int animalId;
 
     public AnimalShelter() {
     }
 
-    public AnimalShelter(List<Animal> animals, int animalId) {
-        this.animals = animals;
+    public AnimalShelter(int animalId) {
         this.animalId = animalId;
-    }
-
-    public List<Animal> getAnimals() {
-        return animals;
-    }
-
-    public void setAnimals(List<Animal> animals) {
-        this.animals = animals;
     }
 
     public int getAnimalId() {
@@ -30,6 +21,15 @@ public class AnimalShelter {
 
     public void setAnimalId(int animalId) {
         this.animalId = animalId;
+    }
+    public void addAnimal(Animal animal){
+        animals.add(animal);
+        animal.treatAnimal();
+        animal.setAnimalNumber(animalId+=1);
+    }
+
+    public void printAnimals(){
+        animals.forEach(animal -> System.out.println(animal));
     }
 
     public void sortAnimals() {
@@ -46,12 +46,17 @@ public class AnimalShelter {
         System.out.println(animals);
     }
     public void printAnimalsNotVaccinated(Disease disease) {
-
+        for(Animal animal : animals) {
+            if (animal.getIsVaccinated().get(disease)==false) {
+                System.out.println(animal.getName() + " with animal number " + animal.getAnimalNumber()
+                        + " is not vaccinated " + disease);
+            }
+        }
     }
 
     public Animal findAnimal(int animalNumber){
         if(animalId == getAnimalId()) {
-            System.out.println("Id correct, your animal is here");
+            System.out.println("You find animal with " + animalNumber);
         }
         return null;
     }
@@ -59,19 +64,26 @@ public class AnimalShelter {
     public Animal findAnimal(String name){
 
         if(animals.contains(name) == name.equals(name)) {
-            System.out.println("Your animal is here");
+            System.out.println("You find animal with " +name);
         }
         return null;
     }
 
     public void treatAnimal(int animalId) {
-        this.animalId = getAnimalId();
+        for (Animal animal : animals) {
+            if (animal.getAnimalNumber()==animalId) {
+                animal.treatAnimal();
+            }
+        }
+
     }
     public void treatAnimal(String name) {
 
     }
     public void treatAllAnimals() {
-
+        for (Animal animal : animals) {
+            animal.treatAnimal();
+        }
     }
 
     public Animal findOldestAnimal(){
